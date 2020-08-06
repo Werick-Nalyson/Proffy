ecb86ec81a1784488632b1b1f328494f90a240f3
Werick-Nalyson <46249623+Werick-Nalyson@users.noreply.github.com>
2020-08-06 16:43:38 -0300

    Front-end finalizado

--- web/src/components/pageHeader/index.tsx
+++ web/src/components/pageHeader/index.tsx
@@ -9,6 +9,7 @@ import './styles.css';
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 // FunctionComponent = FC 
@@ -25,6 +26,7 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
 
             <div className="header-content">
             <strong>{props.title}</strong>
+            { props.description && <p>{props.description}</p> }
         
             {props.children}
             </div>
