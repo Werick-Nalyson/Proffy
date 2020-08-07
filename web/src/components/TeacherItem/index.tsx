c65bbd6518099c7b8e648000066647dabcc7a38f
Werick-Nalyson <46249623+Werick-Nalyson@users.noreply.github.com>
2020-08-06 23:37:15 -0300

    Busca / listagem de classes concluído.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,35 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars0.githubusercontent.com/u/46249623?s=460&u=a3d13f9cbb3f9064bf130d2c61474b15a102c448&v=4" alt="Werick Nalyson" />
-                        <div>
-                            <strong>Werick Nalyson</strong>
-                            <span>Matemática</span>
-                        </div>
-                    </header>
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
 
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada
-                        <br/> <br/>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis iure officia reiciendis accusantium, impedit id sapiente dolores odio, dolor suscipit alias in! Illo molestiae aut eligendi enim minima explicabo aliquid!
-                    </p>
+            <p>{teacher.bio}</p>
 
-                    <footer>
-                        <p>
-                            Preço/hora
-                            <strong>R$ 200,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Entrar em contato" />
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a onClick={createNewConnection} href={`http://wa.me/${teacher.whatsapp}`} target="_blank" type="button">
+                    <img src={whatsappIcon} alt="Entrar em contato" />
+                    Entrar em contato
+                </a>
+            </footer>
+        </article>
     );
 }
 
